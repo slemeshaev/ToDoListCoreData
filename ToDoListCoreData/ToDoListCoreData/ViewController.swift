8ac9c7997de64b6d3953a3885ecd1f70c6750230
slemeshaev <lemeshaevs@gmail.com>
2021-03-03 16:52:48 +0300

    files for project

---
+++ ToDoListCoreData/ToDoListCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCoreData
+//
+//  Created by Станислав Лемешаев on 03.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
